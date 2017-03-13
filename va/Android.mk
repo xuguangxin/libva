38fb34108b4640b61d704c3d72a7e04da16101dd
Linda Yu <linda.yu@intel.com>
2017-03-13 12:35:35 +0800

    libva: build libva into vendor/hw
    Jira: https://01.org/jira/browser/AIA-65
    Test: vainfo
    
    Signed-off-by: Linda Yu <linda.yu@intel.com>

--- va/Android.mk
+++ va/Android.mk
@@ -87,6 +87,8 @@ LOCAL_COPY_HEADERS_TO := libva/va
 
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := libva
+LOCAL_PROPRIETARY_MODULE := true
+LOCAL_MODULE_RELATIVE_PATH := hw
 
 LOCAL_SHARED_LIBRARIES := libdl libdrm libcutils liblog
 
@@ -123,6 +125,8 @@ LOCAL_COPY_HEADERS := va_android.h
 
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := libva-android
+LOCAL_PROPRIETARY_MODULE := true
+LOCAL_MODULE_RELATIVE_PATH := hw
 
 LOCAL_SHARED_LIBRARIES := libva libdrm
 
@@ -150,6 +154,8 @@ LOCAL_COPY_HEADERS := egl/va_egl.h egl/va_backend_egl.h
 
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := libva-egl
+LOCAL_PROPRIETARY_MODULE := true
+LOCAL_MODULE_RELATIVE_PATH := hw
 
 LOCAL_SHARED_LIBRARIES := libva
 
@@ -179,5 +185,7 @@ LOCAL_SHARED_LIBRARIES := libva
 
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := libva-tpi
+CAL_PROPRIETARY_MODULE := true
+LOCAL_MODULE_RELATIVE_PATH := hw
 
 include $(BUILD_SHARED_LIBRARY)
